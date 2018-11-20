52b4545f6a6231ce9e134d1b123a19a821fc7902
Caitlin Cronin <rc0027793@student.riversidecollege.ac.uk>
2018-11-20 09:36:08 +0000

    Task1 part2

--- Hello/Form1.cs
+++ Hello/Form1.cs
@@ -19,7 +19,7 @@ namespace Hello
 
         private void button1_Click(object sender, EventArgs e)
         {
-            MessageBox.Show("Goodbye " + txtboxName.Text);
+            MessageBox.Show("Welcome to Caitlin's program"+ txtboxName.Text);
               
         }
     }
